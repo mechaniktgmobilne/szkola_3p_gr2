851576f860e01574560dae42e5966891b816f866
mechaniktgmobilne <marzkup@gmail.com>
2026-09-11 10:21:44 +0200

    klasa Osoba

---
+++ src/com/company/Osoba.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public abstract class Osoba {
+    //klasa abstrakcyjna nie można utworzyć obiektów tej klasy
+    private String imie;
+    private int wiek;
+
+    //przeciążanie konstruktorów
+    public Osoba(String imie) {
+        this.imie = imie;
+        wiek = 0;
+    }
+
+    public Osoba(String imie, int wiek) {
+        this.imie = imie;
+        this.wiek = wiek;
+    }
+}
